95586917fbe80c26a047ecda0b20b8262dd0de35
Joshua <joshua.oguma@outlook.com>
2020-09-17 20:37:43 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the ContactsKeeper API...' })
+);
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('./api/contacts', require('./routes/api/contacts'));
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
